071bf85f46552bfc3a9e133ee1ba1f93670b1e82
Denis <ddm22026@gmail.com>
2025-04-04 18:28:49 +0300

    all

--- bakcend/server.js
+++ bakcend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 8080;
 const secretKey = 'your_secret_key'; // секрет для подписи JWT
 
 // Массив для хранения пользователей
